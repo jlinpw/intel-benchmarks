4aa149cc3cce5c7b3cb573f17ecbe362e5f8454b
Joey Lin <joey@bruh.myfiosgateway.com>
2023-07-10 13:41:48 -0400

    Change to plotly figures & added test option

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.offline as py
+import plotly.tools as tls
+import sys
 
 
 def process(input_file, output_file, processes):
@@ -15,7 +18,7 @@ def process(input_file, output_file, processes):
 
     # Extract the table data
     table_data = [line.strip() for line in data[start_index:end_index]]
-    if input_file == "pingpong.txt":
+    if input_file.endswith("pingpong.txt"):
         for _ in range(4):
             table_data.pop(0)
         d = [line.split() for line in table_data]
@@ -30,7 +33,7 @@ def process(input_file, output_file, processes):
     # Write the data to a CSV file
     with open(output_file, "w", newline="") as file:
         writer = csv.writer(file)
-        if input_file == "pingpong.txt":
+        if input_file.endswith("pingpong.txt"):
             writer.writerow(["#bytes", "#repetitions", "t[usec]", "Mbytes/sec"])
         else:
             writer.writerow(
@@ -41,7 +44,7 @@ def process(input_file, output_file, processes):
     return [start_index, end_index]
 
 
-def graph(csv, y, figname):
+def graph(csv, y):
     df = pd.read_csv(csv)
 
     fig = plt.figure()
@@ -59,19 +62,33 @@ def graph(csv, y, figname):
     plt.xscale("log")
 
     # Show & save the plot
-    plt.show()
-    fig.savefig(figname)
+    fig = plt.gcf()
+    plotly_fig = tls.mpl_to_plotly(fig)
+    py.iplot(plotly_fig)
 
 
 if __name__ == "__main__":
-    s, e = process("pingpong.txt", "pingpong.csv", 2)
-    # debug
-    #print(s, e)
-    # graph
-    graph("pingpong.csv", "Mbytes/sec", "pingpong.png")
+    if len(sys.argv) > 1:
+        s, e = process("test_files/pingpong.txt", "test_files/pingpong.csv", 2)
+        # debug
+        # print(s, e)
+        # graph
+        graph("test_files/pingpong.csv", "Mbytes/sec")
 
-    t, f = process("alltoall.txt", "alltoall.csv", 4)
-    # debug
-    #print(t, f)
-    # graph
-    graph("alltoall.csv", "t_avg[usec]", "alltoall.png")
+        t, f = process("test_files/alltoall.txt", "test_files/alltoall.csv", 4)
+        # debug
+        # print(t, f)
+        # graph
+        graph("test_files/alltoall.csv", "t_avg[usec]")
+    else:
+        s, e = process("pingpong.txt", "pingpong.csv", 2)
+        # debug
+        # print(s, e)
+        # graph
+        graph("pingpong.csv", "Mbytes/sec")
+
+        t, f = process("alltoall.txt", "alltoall.csv", 4)
+        # debug
+        # print(t, f)
+        # graph
+        graph("alltoall.csv", "t_avg[usec]")
