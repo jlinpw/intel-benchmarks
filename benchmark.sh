39978b51f35c8edbd17ea03262485520583d0b78
Joey Lin <joey@bruh.myfiosgateway.com>
2023-07-11 11:48:13 -0400

    Change xml file & requirements

--- benchmark.sh
+++ benchmark.sh
@@ -11,12 +11,15 @@ pip install -r requirements.txt
 # load the modules
 module load intel-oneapi-compilers/2023.1.0-u3hp4we intel-oneapi-mpi/2021.9.0-hnwuxap
 
+# TODO: use user inputs to use the number of nodes
 # run the benchmark test and pipe the output into a file
 mpirun -np 16 IMB-MPI1 alltoall > alltoall.txt
 mpirun -np 16 IMB-MPI1 pingpong > pingpong.txt
 
 # go to home
 cd
+
+echo $@
 
 # make the graph
 python3 graph.py
