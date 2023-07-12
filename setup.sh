d3068426f81406131c43fc774f38a2219eda8c5b
Joey Lin <joey@bruh.myfiosgateway.com>
2023-07-12 13:09:08 -0400

    Fix lmod

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ remote_node=${resource_1_publicIp}
 code_repo=https://github.com/jlinpw/intel-benchmarks
 abs_path_to_code_repo="/home/${PW_USER}/$(basename $code_repo)"
 
-echo
 echo "REMOTE NODE:  ${remote_node}"
 echo "USER:         ${PW_USER}"
 echo
@@ -17,7 +16,7 @@ git clone ${code_repo}
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
 . $HOME/spack/share/spack/setup-env.sh
 spack install intel-oneapi-mpi intel-oneapi-compilers
-source/user/share/lmod/8.7.7/init/bash
+source /usr/share/lmod/8.7.7/init/bash
 spack module lmod refresh intel-one-api-mpi intel-oneapi-compilers
 export MODULEPATH=$MODULEPATH:$HOME/spack/share/spack/lmod/linux-centos7-x86_64
 EOF
