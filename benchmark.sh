6fc8bcc91e2e7ce62fc65106513cc7e490110f83
Joey Lin <joey@bruh.myfiosgateway.com>
2023-07-12 17:01:26 -0400

    Change to not update python (it doesn't work anyway)

--- benchmark.sh
+++ benchmark.sh
@@ -31,7 +31,7 @@ source /home/ubuntu/spack/share/spack/setup-env.sh
 module use /home/ubuntu/spack/share/spack/lmod/linux-ubuntu22.04-x86_64/Core
 
 # install dependencies
-sudo yum update python3
+# sudo yum update python3
 pip install --upgrade pip
 pip install -r requirements.txt
 
