0d1b152467ff56ad597b328ebe5a3995cc146c2a
Joey Lin <joey@bruh.myfiosgateway.com>
2023-07-13 13:44:09 -0400

    Change regex again

--- benchmark.sh
+++ benchmark.sh
@@ -53,8 +53,8 @@ export MODULEPATH=$MODULEPATH:$HOME/spack/share/spack/lmod/linux-centos7-x86_64
 module avail 
 
 # find the correct modules
-export intel_compilers=$(module avail 2>&1 | grep -o "Core/intel-oneapi-compilers/[^[:space:]]*")
-export intel_mpi=$(module avail 2>&1 | grep -o "Core/intel-oneapi-compilers/[^[:space:]]*")
+export intel_compilers=$(module avail 2>&1 | grep "intel-oneapi-compilers")
+export intel_mpi=$(module avail 2>&1 | grep "intel-oneapi-mpi")
 
 echo "Setting up environment and loading modules:"
 echo ${intel_compilers}
