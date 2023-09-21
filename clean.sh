c168d8276f5b9922779179b62adf9dd30b52e34d
Matt Long <mlong@parallelworks.com>
2023-09-21 11:25:03 -0500

    add -f to rm

--- clean.sh
+++ clean.sh
@@ -5,4 +5,4 @@ source inputs.sh
 #cd 
 #cd $HOME/pw/clusters/${resource_1_name}
 #rm *.csv *.txt *.html
-rm ${HOME}/*.csv ${HOME}/*.txt ${HOME}/*.html
+rm -f ${HOME}/*.csv ${HOME}/*.txt ${HOME}/*.html
