856aaad014457f6d8baa59cf743391b86047e161
Matt Long <mlong@parallelworks.com>
2023-09-21 12:24:05 -0500

    revert setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ branch=inteldemo
 # clone the repository, download spack, setup modules
 git clone --branch ${branch} ${code_repo}
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-#. $HOME/spack/share/spack/setup-env.sh
-. spack/share/spack/setup-env.sh
+. $HOME/spack/share/spack/setup-env.sh
 spack install intel-oneapi-mpi intel-oneapi-compilers
 source /usr/share/lmod/8.7.7/init/bash
 yes | spack module lmod refresh intel-oneapi-mpi intel-oneapi-compilers
